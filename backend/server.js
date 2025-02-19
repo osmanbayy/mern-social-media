ceb59d448bd2c8d3d3afdad4fec209ef76c29d8c
Osman Bay <bayosman342@gmail.com>
2025-02-19 20:56:14 +0300

    Payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({limit: "30mb"})); // to parse req.body
+app.use(express.json({limit: "10mb"})); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
