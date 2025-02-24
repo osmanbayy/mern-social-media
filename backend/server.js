b2bfb2764533691bcaf8858af73036da5549bdac
Osman Bay <bayosman342@gmail.com>
2025-02-24 18:27:29 +0300

    prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import path from "path";
 import express from "express";
 import authRoutes from "./routes/auth_routes.js";
 import userRoutes from "./routes/user_routes.js";
-import postRoutes from "./routes/post_routes.js"
-import notificationRoutes from "./routes/notification_routes.js"
+import postRoutes from "./routes/post_routes.js";
+import notificationRoutes from "./routes/notification_routes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connect_mongodb from "./database/connect_mongodb.js";
@@ -19,15 +20,25 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({limit: "10mb"})); // to parse req.body
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/post", postRoutes)
+app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
